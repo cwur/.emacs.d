cabdc925184f4eab6c51a36f02086a7d71bad7a8
cwurexit <cwur@mailbox.org>
2018-09-29 12:32:43 +0200

    Sanitization (moved (package-initialize) + removed superfluous lines.

--- init.el
+++ init.el
@@ -1,17 +1,13 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;;(package-initialize)
+(package-initialize)
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org-bullets which-key try use-package))))
+ '(package-selected-packages (quote (org org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
