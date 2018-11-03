eb3bbce3ecb543615f43aff0181e3451367dd944
cwurexit <cwur@mailbox.org>
2018-11-03 20:24:06 +0100

    Added anki-editor. Changed org-brain settings. Formatting.

--- init.el
+++ init.el
@@ -7,10 +7,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org org-bullets which-key try use-package))))
+ '(package-selected-packages
+   (quote
+    (anki-editor org-plus-contrib org-drill org org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
