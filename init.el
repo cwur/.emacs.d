1bdb55eeb6ca83f46afa79ac9b5385e06eaa37bc
cwurexit <cwur@mailbox.org>
2018-12-09 18:07:25 +0100

    Added packages "py-autopep8" and "auto-minor-mode".

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flycheck elpy better-defaults material-theme htmlize anki-editor org-plus-contrib org-drill org org-bullets which-key try use-package))))
+    (auto-minor-mode py-autopep8 flycheck elpy better-defaults material-theme htmlize anki-editor org-plus-contrib org-drill org org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
